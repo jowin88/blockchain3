04e253446fa3f4939d67051054d2d6048596b676
Jowin <jowin88@gmail.com>
2019-02-01 23:01:05 +0800

    last saved for semantic n foundation
    i save this last coz i hate using them. so much limitation

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # blockchain3
 I am using semantic-ui for the ewallet.
-Run the following.
+For set up, run the following at its folder.
 #npm install -g gulp
 #npm install semantic-ui --save
 #cd semantic/
 #gulp build
+
+Start another cmd and install semantic-ui dropdown
+#npm install semantic-ui-dropdown
+
+To make the website starts working, run the following.
+#npm install -g gulp
+#npm install semantic-ui --save
+
+Run at different cmd.
+#node index.js
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var miner_address = "q3nf394hjg-random-miner-address-34nf3i4nflkn3oi";
 // other node in the network
 // so that we can communicate
 // with them
-peer_nodes = ["http://localhost:8080","http://localhost:8081","http://localhost:8082"];
+peer_nodes = ["http://localhost:8080"];
 current_node = "http://localhost:8080";
 // A variable to deciding if we're mining or not
 mining = true;
@@ -24,40 +24,47 @@ if(blockchain.length == 0)
 else{previous_block = blockchain[blockchain.length - 1];}
 function consensus(events)
 {
-	console.log("\n consensus started \n");
+	console.log("\n start running consensus \n");
 	// Get the blockchains of every
 	// other node
 	var other_chains = [];
 	done = 0;
-	for(i=0;i<peer_nodes.length;i++)
+	// the peer nodes should always be more than 1
+	if(peer_nodes.length>1)
 	{
-		if(peer_nodes[i]!=current_node) // connection same port will cause error
+		for(i=0;i<peer_nodes.length;i++)
 		{
-			data = "";
-			// Get their chains using a GET request
-			var request = http.get(peer_nodes[i]+"/blocks", function (req, res) {
-				req.on('data', function (chunk) {
-					data = JSON.parse(chunk);
-				});
-				req.on('end', function () {
-					// Convert the JSON object to a Python dictionary
-					// Add it to our list
-					find_new_chain(data);
-				});
-			}).on('error', function(e){});
+			if(peer_nodes[i]!=current_node) // connection same port will cause error
+			{
+				data = "";
+				// Get their chains using a GET request
+				var request = http.get(peer_nodes[i]+"/blocks", function (req, res) {
+					req.on('data', function (chunk) {
+						data = JSON.parse(chunk);
+					});
+					req.on('end', function () {
+						// Convert the JSON object to a Python dictionary
+						// Add it to our list
+						find_new_chain(data);
+					});
+				}).on('error', function(e){});
+			}
 		}
 	}
-	function find_new_chain(data2)
+	else{find_new_chain(other_chains, "single node");}
+	function find_new_chain(data2, status="")
 	{
+		console.log("\n triggered find_new_chain \n");
 		++done;
 		if(blockchain.length < data2.length)
 		{
 			blockchain = data2;
 		}
-		if(done == peer_nodes.length - 1)
+		if(done == peer_nodes.length - 1 || status=="single node")
 		{
 			if(events == "mine")
 			{
+				console.log("the mine trigger is started");
 				// trigger the mine
 				var mine = require("./mine").mine;
 				miner = mine(blockchain, this_nodes_transactions, miner_address);
@@ -65,7 +72,7 @@ function consensus(events)
 				this_nodes_transactions = miner[1];
 				mined = miner[2];
 				console.log("Mined : "+JSON.stringify(mined));
-				result = "Mining submission successful\n"
+				result = "Mining submission successful\n";
 			}
 		}
 	}	
@@ -75,9 +82,10 @@ consensus("start");
 // this node has in a list
 var this_nodes_transactions = [];
 result = "";
-
+responsed = "no";
 //create a server object:
 http.createServer(function (req, res) {
+	ended="no";
 	if(req.method == "POST")
 	{
 		if(req.url=="/transaction")
@@ -91,8 +99,9 @@ http.createServer(function (req, res) {
 				var post = qs.parse(body);
 				var transaction = require("./transaction").transaction;
 				this_nodes_transactions = transaction(this_nodes_transactions, post);
-				result = "Transaction submission successful\n";
+				consensus("mine");
 			});
+			result = "<script>alert('Your transaction is submitted');window.history.back();</script>";
 		}
 		else{result="No result";}
 	}
@@ -101,6 +110,7 @@ http.createServer(function (req, res) {
 		link = url.parse(req.url, true);
 		if(link.pathname=="/mine")
 		{
+			console.log("\n mine request started \n");
 			consensus("mine");
 		}
 		else if(link.pathname=="/blocks")
@@ -109,9 +119,22 @@ http.createServer(function (req, res) {
 			result = get_blocks(blockchain);
 			console.log(result);
 		}
+		else if(link.pathname=="/index")
+		{
+			var fs = require('fs');ended="yes";
+			fs.readFile('index.html','utf8', function(err, data) {
+				res.writeHead(200, {'Content-Type': 'text/html'});
+				res.write(data); //write a response to the client
+				res.end(); //end the response
+			});
+		}
 		else{result="No result";}
 	}
 	else{result="No result";}
-	res.write(result); //write a response to the client
-	res.end(); //end the response
+	if(ended=="no")
+	{
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(result); //write a response to the client
+		res.end(); //end the response
+	}
 }).listen(8080); //the server object listens on port 8080
